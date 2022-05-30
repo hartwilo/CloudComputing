4910874993f0751fc343561ae0b38ea3575245af
Lorenz Hartwig <lorenz.hartwig94@gmail.com>
2022-05-30 15:06:39 +0200

    Added Email Notification

---
+++ src/test/java/de/hft/stuttgart/de/UserControllerTest.java
@@ -0,0 +1,22 @@
+package de.hft.stuttgart.de;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserControllerTest {
+
+    @Test
+	void getCurrentUserTest() {
+
+        String user = System.getProperty("user.name");
+
+        assertEquals("Lorenz", user);
+
+	}
+
+
+    
+}
